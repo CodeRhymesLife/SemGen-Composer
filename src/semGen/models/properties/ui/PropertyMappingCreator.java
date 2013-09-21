7127ae61038e8cf6f4fe9c65e16c75e2158bfa0c
Ryan James <rcjames1004@gmail.com>
2013-09-21 10:32:35 -0700

    Create mapped property
    -Select a properties to map from 2 source models
    -Add property mapping to merged model

--- src/semGen/models/properties/ui/PropertyMappingCreator.java
+++ src/semGen/models/properties/ui/PropertyMappingCreator.java
@@ -15,7 +15,7 @@ import ui.FlyoutPosition;
 /*
  * Creates a property mapping
  */
-public class PropertyMappingCreator implements ModelPropertyListener {
+public class PropertyMappingCreator {
 	// Flyout used to select properties
 	private static PropertyMappingsFlyout _propertiesFlyout;
 
@@ -25,11 +25,19 @@ public class PropertyMappingCreator implements ModelPropertyListener {
 	// Model to create property mapping for
 	private MergedModel _mergedModel;
 	
+	// Property selection listeners
+	private ModelPropertySelectedListener _model1Listener;
+	private ModelPropertySelectedListener _model2Listener;
+	
 	public PropertyMappingCreator(MergedModel mergedModel){
 		if(mergedModel == null)
 			throw new NullPointerException("mergedModel");
 		
 		_mergedModel = mergedModel;
+		
+		// Create property selection listeners
+		_model1Listener = new ModelPropertySelectedListener(this);
+		_model2Listener = new ModelPropertySelectedListener(this);
 		
 		// If the flyout hasn't been created yet create it
 		if(_propertiesFlyout == null){
@@ -53,26 +61,85 @@ public class PropertyMappingCreator implements ModelPropertyListener {
 		propertyMappingComponenetParent.validate();
 		propertyMappingComponenetParent.repaint();
 		
-		// Pick from source 1 first
-		ModelPropertiesTable propertiesTable = _propertiesFlyout.getPropertiesTable();
-		propertiesTable.setProperties(_mergedModel.getSourceModel1().getProperties());
-		
-		// Listen for the property selection event
-		_propertiesFlyout.getPropertiesTable().setPropertySelectionListener(this);
-		
-		// Show the flyout around the first property component
-		_propertiesFlyout.showAroundComponent(_propertyMappingComponent, FlyoutPosition.Left);
+		// Select the next property
+		this.selectNextProperty();
 	}
 	
 	/*
-	 * Called when a model property is selected from the property list.
-	 * 
-	 * (non-Javadoc)
-	 * @see semGen.models.properties.ModelPropertyListener#propertyEvent(semGen.models.properties.IModelProperty)
+	 * Selects the next property. If properties from both models have been
+	 * selected a new merged model property is created and added to the merged model
 	 */
-	@Override
-	public void propertyEvent(IModelProperty property) {
-		throw new UnsupportedOperationException("Handle property selected");
+	private void selectNextProperty(){
+		// If we haven't selected a property from the first
+		// list the properties for selection
+		if(_model1Listener.getSelectedProperty() == null){
+			listPropertiesForSelection(_mergedModel.getSourceModel1(), _model1Listener, FlyoutPosition.Left);
+		}
+		// Otherwise if we havent selected a property from the
+		// second model list the properties for selection
+		else if(_model2Listener.getSelectedProperty() == null){
+			listPropertiesForSelection(_mergedModel.getSourceModel2(), _model2Listener, FlyoutPosition.Right);
+		}
+		// Otherwise if both properties have been selected create the merged property
+		// and add it to the merged model
+		else{
+			// Hide the properties flyout
+			_propertiesFlyout.setVisible(false);
+			
+			// Stop listening for the property selection event
+			_propertiesFlyout.getPropertiesTable().setPropertySelectionListener(null);
+			
+			// Add the new mapping to the merged model
+			_mergedModel.addProperty(new MergedModelProperty(_model1Listener.getSelectedProperty(), _model2Listener.getSelectedProperty()));
+		}
+		
+	}
+	
+	/*
+	 * Lists the properties in the given model for selection
+	 */
+	private void listPropertiesForSelection(Model model, ModelPropertySelectedListener listener, FlyoutPosition position){
+		ModelPropertiesTable propertiesTable = _propertiesFlyout.getPropertiesTable();
+		propertiesTable.setProperties(model.getProperties());
+		
+		// Listen for the property selection event
+		_propertiesFlyout.getPropertiesTable().setPropertySelectionListener(listener);
+		
+		// Show the flyout around the first property component
+		_propertiesFlyout.showAroundComponent(_propertyMappingComponent, position);
+	}
+	
+	private class ModelPropertySelectedListener implements ModelPropertyListener{
+		// Mapping creator
+		private PropertyMappingCreator _mappingCreator;
+		
+		// Selected Property
+		private IModelProperty _selectedProperty;
+		
+		public ModelPropertySelectedListener(PropertyMappingCreator mappingCreator){
+			_mappingCreator = mappingCreator;
+		}
+		
+		/*
+		 * Gets the selected property
+		 */
+		public IModelProperty getSelectedProperty(){
+			return _selectedProperty;
+		}
+		
+		/*
+		 * Called when a property has been selected from the table
+		 * 
+		 * (non-Javadoc)
+		 * @see semGen.models.properties.ModelPropertyListener#propertyEvent(semGen.models.properties.IModelProperty)
+		 */
+		@Override
+		public void propertyEvent(IModelProperty property) {
+			_selectedProperty = property;
+			
+			// Select the next property
+			_mappingCreator.selectNextProperty();
+		}
 	}
 	
 	/*
