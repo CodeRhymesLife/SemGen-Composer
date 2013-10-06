fa1b2afc5b7ad43427c05bb7cd30eb4e39ddeab4
Ryan James <rcjames1004@gmail.com>
2013-10-06 03:44:23 -0700

    Add properties to combobox
    -Add combobox renderer that know how to render properties
    -Add properties to combox and render then appropriately

--- src/semGen/models/properties/ui/PropertyMappingComponent.java
+++ src/semGen/models/properties/ui/PropertyMappingComponent.java
@@ -1,5 +1,9 @@
 package semGen.models.properties.ui;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.ListCellRenderer;
 
 import semGen.models.properties.IModelProperty;
 import semGen.models.properties.MergedModelProperty;
@@ -11,23 +15,44 @@ import ui.FlyoutPosition;
 import java.awt.Component;
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 import javax.swing.JComboBox;
 
 
-public class PropertyMappingComponent extends JPanel {
+public class PropertyMappingComponent extends JPanel implements ActionListener {
 	// Property components
 	protected PropertyComponent _property1Component;
 	protected PropertyComponent _property2Component;
 	protected JComboBox _comboBoxPropertySelector;
 	
+	// Merged model property
+	private MergedModelProperty _mergedModelProperty;
+	private static final String ChooseRepresentationString = "Choose Representation";
+	
 	public PropertyMappingComponent(MergedModelProperty mergedModelProperty) {
 		this();
 
+		// Save the merged model property
+		_mergedModelProperty = mergedModelProperty;
+		
 		// Set the properties on the property components
 		_property1Component.setProperty(mergedModelProperty.getProperty1());
 		_property2Component.setProperty(mergedModelProperty.getProperty2());
+		
+		// Set the properties on the combobox
+		DefaultComboBoxModel model = (DefaultComboBoxModel)_comboBoxPropertySelector.getModel();
+		model.addElement(ChooseRepresentationString);
+		model.addElement(mergedModelProperty.getProperty1());
+		model.addElement(mergedModelProperty.getProperty2());
+		
+		// If we have a source model select it
+		if(mergedModelProperty.getSourceProperty() != null){
+			model.setSelectedItem(mergedModelProperty.getSourceProperty());
+		}
 	}
 	
 	protected PropertyMappingComponent() {
@@ -40,7 +65,10 @@ public class PropertyMappingComponent extends JPanel {
 		panelLeftSeparator.setBackground(Color.BLACK);
 		add(panelLeftSeparator);
 		
-		_comboBoxPropertySelector = new JComboBox();
+		_comboBoxPropertySelector = new JComboBox(new DefaultComboBoxModel());
+		PropertyMappingComboBoxRenderer renderer = new PropertyMappingComboBoxRenderer();
+		_comboBoxPropertySelector.setRenderer(renderer);
+		_comboBoxPropertySelector.setMaximumRowCount(3);
 		add(_comboBoxPropertySelector);
 		
 		JPanel panelRightSeparator = new JPanel();
@@ -50,5 +78,68 @@ public class PropertyMappingComponent extends JPanel {
 		_property2Component = new PropertyComponent();
 		_property2Component.setAlignmentX(Component.RIGHT_ALIGNMENT);
 		add(_property2Component);
+	}
+	
+	/**
+	 * Sets the selected property as the source property
+	 * on the merged model property
+	 */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+        JComboBox cb = (JComboBox)e.getSource();
+        Object selectedItem = cb.getSelectedItem();
+        
+        // If we selected a model property add it and remove
+        // the choose representation item
+        if(selectedItem instanceof IModelProperty){
+        	 _mergedModelProperty.setSourceProperty((IModelProperty)cb.getSelectedItem());
+        }
+    }
+	
+	/**
+	 * Renders combobox elements
+	 * 
+	 * @author rjames
+	 *
+	 */
+	private class PropertyMappingComboBoxRenderer extends JLabel implements ListCellRenderer {
+		public PropertyMappingComboBoxRenderer() {
+			setOpaque(true);
+			setVerticalAlignment(CENTER);
+		}
+
+		/**
+		 * Renders the combobox value
+		 */
+		public Component getListCellRendererComponent(
+		                    JList list,
+		                    Object value,
+		                    int index,
+		                    boolean isSelected,
+		                    boolean cellHasFocus)
+		{
+			
+			if (isSelected) {
+				setBackground(list.getSelectionBackground());
+				setForeground(list.getSelectionForeground());
+			} else {
+				setBackground(list.getBackground());
+				setForeground(list.getForeground());
+			}
+			
+			String displayText;
+			if(value instanceof String)
+				displayText = (String)value;
+			else{
+				IModelProperty property = (IModelProperty)value;
+				
+				// Build the string to show in the combobox as follows:
+				// <property name> (<property parent model name>)
+				displayText = String.format("%s (%s)", property.getName(), property.getParentModel().getName());
+			}
+			
+			setText(displayText);
+			return this;
+		}
 	}
 }
