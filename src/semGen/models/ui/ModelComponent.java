e26b4e2573d7e27288802831548753b2f5f4a686
rcjames1004 <rcjames1004@gmail.com>
2013-11-23 18:26:42 -0800

    Close flyout when model deleted

--- src/semGen/models/ui/ModelComponent.java
+++ src/semGen/models/ui/ModelComponent.java
@@ -5,6 +5,7 @@ import java.awt.datatransfer.StringSelection;
 import java.awt.datatransfer.Transferable;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionAdapter;
@@ -93,6 +94,13 @@ public class ModelComponent extends JPanel implements IModelComponent {
 		deleteButtonContainer.setOpaque(false);
 		add(deleteButtonContainer);
 		deleteButtonContainer.setLayout(null);
+		
+		// Close flyout when delete button clicked
+		addDeleteActionListener(new MouseAdapter() { 
+			public void mousePressed(MouseEvent me) { 
+				Flyout.setVisible(false);
+			} 
+	    });
 		
 		deleteButtonContainer.add(_lblDelete);
 		
